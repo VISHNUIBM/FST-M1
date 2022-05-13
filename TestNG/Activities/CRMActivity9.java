3a19cd097f3ad006d7c94ae9279b2ab2d20217e8
VISHNUIBM <105268780+VISHNUIBM@users.noreply.github.com>
2022-05-13 13:00:01 +0530

    Add files via upload

---
+++ TestNG/Activities/CRMActivity9.java
@@ -0,0 +1,80 @@
+package CRM.PROJECT;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CRMActivity9 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        //Create a new instance of the Firefox driver
+        driver = new FirefoxDriver();
+
+        //Open browser
+        driver.get("https://alchemy.hguy.co/crm/");
+    }
+
+    @Test
+    public void exampleTestCase() {
+        // Check the title of the page
+        WebElement userName = driver.findElement(By.xpath("//*[@id='user_name']"));
+        userName.sendKeys("admin");
+        WebElement password1 = driver.findElement(By.xpath("//*[@id='username_password']"));
+        password1.sendKeys("pa$$w0rd");
+
+        WebElement submit = driver.findElement(By.xpath("//*[@id='bigbutton']"));
+        submit.click();
+
+        try{
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException ie){
+        }
+
+        WebElement Sales = driver.findElement(By.xpath("//*[@id='grouptab_0']"));
+        Sales.click();
+
+        try{
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException ie){
+        }
+
+        WebElement Account = driver.findElement(By.xpath("//*[@id='moduleTab_9_Leads']"));
+        Account.click();
+
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException ie){
+        }
+
+        for (int i=1;i<11;i++) {
+            String xpath1 = "//table/tbody/tr[";
+            String xpath2 = "]/td[3]/b/a";
+            String XpathVariable1 = xpath1 + i + xpath2;
+            xpath1 = "//table/tbody/tr[";
+            xpath2 = "]/td[8]/a";
+            String XpathVariable2 = xpath1 + i + xpath2;
+            WebElement Table1 = driver.findElement(By.xpath(XpathVariable1));
+            WebElement Table2 = driver.findElement(By.xpath(XpathVariable2));
+            // System.out.println(XpathVariable);
+            //String array[][] =
+            System.out.println("Name:  " + Table1.getText() + "  User: "+ Table2.getText());
+
+        }
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        //Close the browser
+        driver.quit();
+    }
+
+}
